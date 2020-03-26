f2e89c9543555ca3e325f4a1a4c08182596d8525
yaroslavsuslov <yaroslavsuslov@gmail.com>
2020-03-26 13:09:05 +0300

    proxy pattern

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,2 +1,27 @@
-public class Order {
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Order implements OrderInterface{
+
+    private int id;
+    private User user;
+    private List<Sample> samples;
+
+    public Order(List<Sample> samples, User user) {
+        id = ThreadLocalRandom.current().nextInt(); // replacing logic. we would get id by autoincrement from DB
+        this.user = user;
+        this.samples = samples;
+    }
+
+    public List<Sample> getSamples() {
+        return samples;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
