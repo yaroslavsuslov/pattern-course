68b27c9e7df6a454993ab4c761a575534b731e8c
yaroslavsuslov <yaroslavsuslov@gmail.com>
2020-04-08 20:09:43 +0300

    hw7

---
+++ src/main/java/mvc/CategoryService.java
@@ -0,0 +1,56 @@
+package mvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Category> findAllWithoutProducts() {
+        return categoryRepository.findAllWithoutProducts();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Category> findByIdWithProducts(Long id) {
+        return categoryRepository.findByIdWithProducts(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Category> findByIdWithoutProducts(Long id) {
+        return categoryRepository.findByIdWithoutProducts(id);
+    }
+
+    @Transactional
+    public void save(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Transactional
+    public void saveByName(Category category) {
+        Long id = categoryRepository.findIdByName(category.getName()).
+                orElse(null);
+        if (id == null) {
+            save(category);
+        } else {
+            category.setId(id);
+            save(category);
+        }
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
